2d5fdb272c9a200774b3b2286cd7274f3c32fee4
adiatul16 <adiatul16@gmail.com>
2020-11-08 20:53:34 +0530

    miatoll: Set correct inherit vendor path

--- derp_miatoll.mk
+++ derp_miatoll.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_miatoll
