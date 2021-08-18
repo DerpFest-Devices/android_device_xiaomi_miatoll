f08f139145cf21c1cdc835502fcc90991865b989
klozz <klozz@TheXPerienceProject.org>
2021-08-18 14:30:37 +0200

    miatoll: Move fp to walley
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- init/init.cpp
+++ init/init.cpp
@@ -70,29 +70,29 @@ void load_device_properties() {
     if (hwname == "curtana") {
         if (region == "Global_TWO") {
             set_device_props(
-                "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-                "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+                "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys",
+                "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys",
                 "Redmi", "curtana", "Redmi Note 9S");
         } else if (region == "India") {
             set_device_props(
-                "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-                "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+                "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys",
+                "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys",
                 "Redmi", "curtana", "Redmi Note 9 Pro");
         }
     } else if (hwname == "excalibur") {
         set_device_props(
-                "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-                "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+                "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys",
+                "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys",
                 "Redmi", "excalibur", "Redmi Note 9 Pro Max");
     } else if (hwname == "gram") {
         set_device_props(
-                "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-                "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+                "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys",
+                "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys",
                 "POCO", "gram", "POCO M2 Pro");
     } else if (hwname == "joyeuse") {
         set_device_props(
-                "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-                "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+                "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys",
+                "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys",
                 "Redmi", "joyeuse", "Redmi Note 9 Pro");
     }
 }
